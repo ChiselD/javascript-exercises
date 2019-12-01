2949da3c1b110f3537224feefb5784fa1127c153
Jinx <chisel.delahanty@gmail.com>
2019-12-01 21:29:42 +0100

    Complete repeatString challenge

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str, mult) {
+	if (mult < 0) return "ERROR";
+	let result = "";
+	for (let i = 0; i < mult; i++) {
+		result += str;
+	}
+	return result;
 }
 
 module.exports = repeatString
