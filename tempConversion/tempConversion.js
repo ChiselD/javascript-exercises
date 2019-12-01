8da89ad3de5bb3832e80b184bc4b9bb7b021681c
Jinx <chisel.delahanty@gmail.com>
2019-12-01 22:34:07 +0100

    Complete tempConversion challenge

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(n) {
+	const a = (n - 32) * (5/9);
+	const b = parseFloat(a.toFixed(1));
+	return b;
 }
 
-const ctof = function() {
-
+const ctof = function(n) {
+	const a = (n * (9/5)) + 32;
+	const b = parseFloat(a.toFixed(1));
+	return b;
 }
 
 module.exports = {
