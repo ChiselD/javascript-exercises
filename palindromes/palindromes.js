152fa737d51554408d9a2136cd92a567d8a62ad9
Jinx <chisel.delahanty@gmail.com>
2019-12-08 01:24:56 +0100

    Complete palindromes challenge

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function() {
-
+const palindromes = function(inputStr) {
+	const str = inputStr.replace(/[^0-9a-z]/gi, '').toLowerCase();
+	const reversed = str.split('').reverse().join('');
+	for (let i = 0; i < str.length; i++) {
+		if (str[i] !== reversed[i]) {
+			return false;
+		}
+	}
+	return true;
 }
 
 module.exports = palindromes
