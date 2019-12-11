4e85644dca1b51d1936b5321d9846ab4ed1b6c7d
Jinx <chisel.delahanty@gmail.com>
2019-12-11 23:19:33 +0100

    Complete fibonacci challenge

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+	if (n < 0) {return "OOPS";}
+	let x = 0, y = 1, z = 0;
+	for (let i = 0; i < n; i++) {
+		z = x + y; // x = 0, y = 1, z = 1
+		x = y;
+		y = z;
+	}
+	return x;
 }
 
 module.exports = fibonacci
