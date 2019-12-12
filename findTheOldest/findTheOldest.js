2f8286cdabd6eea9ef43b6c43d3be9bf57181fdf
Jinx <chisel.delahanty@gmail.com>
2019-12-12 20:26:08 +0100

    Complete findTheOldest challenge

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-let findTheOldest = function() {
-
+let findTheOldest = function(arr) {
+	let oldestAge = 0;
+    let oldestPerson = arr[0];
+    for (let i = 0; i < arr.length; i++) {
+      let person = arr[i];
+      let age;
+      if (person.yearOfDeath) {
+        age = person.yearOfDeath - person.yearOfBirth;
+      } else {
+        age = new Date().getFullYear() - person.yearOfBirth;
+      }
+      if (age > oldestAge) {
+        oldestAge = age;
+        oldestPerson = person;
+      }
+    }
+    return oldestPerson;
 }
 
 module.exports = findTheOldest
